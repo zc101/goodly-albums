51daa5aecef9d57bdc5abc74f68845c9121e2122
Zach Caldwell <zach.caldwell101@gmail.com>
2020-03-20 20:41:36 -0600

    Implement basic user/password functions and a users table

--- mysql-scripts/create-tables.sql
+++ mysql-scripts/create-tables.sql
@@ -6,5 +6,15 @@ CREATE TABLE visitors (
   visitor_id   INT         NOT NULL  AUTO_INCREMENT
 , ip_addr      VARCHAR(40) NOT NULL
 , visits_count INT         NOT NULL
-  PRIMARY KEY ( visitor_id )
+, CONSTRAINT visitors_pk PRIMARY KEY ( visitor_id )
 );
+
+CREATE TABLE users (
+  user_id       INT         NOT NULL  AUTO_INCREMENT
+, user_name     VARCHAR(20) NOT NULL
+, password_hash VARCHAR(64) NOT NULL
+, password_salt VARCHAR(32) NOT NULL
+, CONSTRAINT users_pk PRIMARY KEY ( user_id )
+);
+
+ALTER TABLE users AUTO_INCREMENT = 1000;
