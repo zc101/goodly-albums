b9d962ee3cf3450ce05bf86da8648e47dd3b9704
Zach Caldwell <zach.caldwell101@gmail.com>
2020-03-30 10:17:39 -0600

    Use Knex and async/await to obliterate much nastiness

--- route/visitors.js
+++ route/visitors.js
@@ -2,83 +2,62 @@
 
 const db = require('../lib/db');
 
-function bumpVisitors(ip, cb) {
+async function bumpVisitors(ip) {
   if (ip) {
     // Use a regex match to be safe
     const ipExpr = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
     let ipList = ip.match(ipExpr);
 
     if (ipList) {
-      let ip = db.escape(ipList[0]); // Some extra paranoia
-      let qUpdate = ' \
-        IF EXISTS(SELECT visitor_id FROM visitors WHERE ip_addr = ' + ip + ') THEN \
-          UPDATE visitors \
-            SET visits_count = visits_count + 1 \
-            WHERE ip_addr = ' + ip + '; \
-        ELSE \
-          INSERT INTO visitors \
-            (ip_addr, visits_count) \
-          VALUES \
-            (' + ip + ', 1); \
-        END IF;';
+      try {
+        let ipAddr = ipList[0];
 
-      let qVisitors = 'SELECT COUNT(*) AS unique_visitors FROM visitors;';
-      let qVisits = 'SELECT SUM(visits_count) AS total_visits FROM visitors;';
-
-      // Cascade queries using a single connection
-      db.getConnection(function(err, connection) {
-        if (err) {
-          console.error(error);
-          cb(null);
-          return;
+        let ipSearch = await db.select('visits_count').from('visitors').where('ip_addr', ipAddr);
+        if (ipSearch && ipSearch.length) {
+          // Found an existing entry; update it
+          await db('visitors')
+          .where('ip_addr', ipAddr)
+          .update({visits_count: ipSearch[0].visits_count + 1});
+        }
+        else {
+          // Insert a new IP entry
+          let visitorRow = {
+            ip_addr: ipAddr
+          , visits_count: 1
+          };
+          await db('visitors').insert(visitorRow);
         }
 
-        try {
-          connection.query(qUpdate, function (err, results, fields) {
-            if (err) throw err;
-            connection.query(qVisitors, function (err, results, fields) {
-              if (err) throw err;
-              let unique_visitors = results[0].unique_visitors;
-              connection.query(qVisits, function (err, results, fields) {
-                if (err) throw err;
-                let vals = {
-                  visitors: unique_visitors
-                , visits: results[0].total_visits
-                };
+        let results = await db('visitors').count('* as unique_visitors').sum('visits_count as total_visits');
+        if (results && results.length) {
+          let vals = {
+            visitors: results[0].unique_visitors
+          , visits: results[0].total_visits
+          };
 
-                connection.release();
-                cb(vals);
-              });
-            });
-          });
+          return vals;
         }
-
-        catch(err) {
-          connection.release();
-          console.error(error);
-          cb(null);
-          return;
-        }
-      });
+      }
+      catch(err) {
+        console.error("getVisitors: Caught while running queries:");
+        console.error(err);
+      }
     }
-    else { // IP regex match failed
+    else // IP regex match failed
       console.error("getVisitors: Received bad IP '" + ip + "'");
-      cb(null);
-    }
   }
-  else { // !ip
+  else // !ip
     console.error("getVisitors: No IP given");
-    cb(null);
-  }
+
+  return null;
 }
 
 
 // Route handler
-module.exports = function (req, res) {
-  bumpVisitors(req.ip, function(vals) {
-    if(vals)
-      res.send('This site has been viewed ' + vals.visits + ' times by ' + vals.visitors + ' visitors.');
-    else
-      res.status(400).send();
-  });
+module.exports = async function (req, res) {
+  let vals = await bumpVisitors(req.ip);
+  if(vals)
+    res.send('This site has been viewed ' + vals.visits + ' times by ' + vals.visitors + ' visitors.');
+  else
+    res.status(400).send();
 };
