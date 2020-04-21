54f02c0ac6bb7052a3628bd5d5c8215f6608507a
Zach Caldwell <zach.caldwell101@gmail.com>
2020-04-21 15:50:47 -0600

    Tweak secure-check to not return HTML

--- route/secure.js
+++ route/secure.js
@@ -1,10 +1,12 @@
-// Redirect to the login form if we can't find a decrypted auth token
+// Bail out of the routing chain if we can't find a decrypted auth token
+
+'use strict';
 
 module.exports = function (req, res, next) {
-  let auth = res.locals.cookieTokens && res.locals.cookieTokens.auth_token;
+  let auth = (res.locals.cookieTokens && res.locals.cookieTokens.auth_token && (typeof(auth.userID) === 'number'));
 
   if (auth)
     next();
   else
-    res.status(200).send('<script>window.location.href = "/en/user_login.html?return_to=" + encodeURIComponent(window.location.href)</script>');
+    res.status(403).end();
 };
