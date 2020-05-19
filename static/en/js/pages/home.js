f64e78e56c0d1c2910dbf006905895a096177c54
Zach Caldwell <zach.caldwell101@gmail.com>
2020-05-19 13:21:51 -0600

    Implement homepage

---
+++ static/en/js/pages/home.js
@@ -0,0 +1,33 @@
+// Homepage functionality
+
+'use strict';
+
+// Shared variables
+var baseContents = $("#album-list").html();
+
+// Load list of albums
+function loadAlbums() {
+  requestJSON("/en/srv/public_albums", function(data) {
+    var newContents = '';
+    for (var i = 0; i < data.length; ++i) {
+      var album = data[i];
+      var card = '<div class="card">';
+      if (album.album_cover)
+        card = card + '<img src="' + album.album_cover + '" class="card-img-top" alt="Album thumbnail">';
+      else
+        card = card + '<div class="empty-cover">(No Preview Available)</div>';
+
+      card = card + '<div class="card-body"><h5 class="card-title">' + album.album_name + '</h5>';
+
+      if (album.album_desc)
+        card = card + '<p class="card-text">' + album.album_desc + '</p>';
+
+      newContents = newContents + card + '</div></div>';
+    }
+    $("#album-list").html(baseContents + newContents);
+    $("#alert_msg").setAlertClass("hidden");
+  });
+};
+
+// Perform an initial load
+loadAlbums();
