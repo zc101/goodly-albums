f5fafb9241e251738bca88473333b7542aed9ac8
Zach Caldwell <zach.caldwell101@gmail.com>
2020-11-05 13:55:43 -0700

    Update database creation script

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -3,10 +3,12 @@
 sudo mysqladmin -p create GOODLYALBUMS
 
 sudo mysql -Be " \
+  uninstall plugin validate_password; \
+  CREATE USER 'goodlyalbums-app'@'localhost' \
+  IDENTIFIED WITH mysql_native_password BY 'goodlyalbums-app'; \
   GRANT SELECT,INSERT,UPDATE,DELETE \
   ON GOODLYALBUMS.* \
-  TO 'goodlyalbums-app'@'localhost' \
-  IDENTIFIED BY 'goodlyalbums-app'; \
+  TO 'goodlyalbums-app'@'localhost'; \
   FLUSH PRIVILEGES;"
 
 # Others: # CREATE,DROP,ALTER,FILE
